fd431235b2ee649c49fb288064270410f85760a5
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2024-12-25 13:26:11 +0600

    dta await params fix

--- src/app/services/api/[id]/route.js
+++ src/app/services/api/[id]/route.js
@@ -4,7 +4,8 @@ export const GET = async (request, {params}) => {
     const db =await connectDB()
     const servicesCollections = db.collection('services')
     try {
-        const service = await servicesCollections.findOne({_id : params.id})
+        const { id } = await params;
+        const service = await servicesCollections.findOne({_id : id})
         return Response.json({service})
     } catch (error) {
         console.log(error)
